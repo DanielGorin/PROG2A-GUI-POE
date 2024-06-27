adf76c98fb58894f9f2a063f55a2fec42c89cbe3
DanielGorin <daniel.l.gorin@gmail.com>
2024-06-27 10:55:40 +0200

    Added the pages and classes but not filled

---
+++ RecipeObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2A_GUI_POE
+{
+    internal class RecipeObject
+    {
+    }
+}
